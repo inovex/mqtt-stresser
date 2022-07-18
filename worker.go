9dc5a92566c68f4662109da614cdd42013c723bd
ming <mwei0210@gmail.com>
2022-07-18 16:04:41 +0800

    mqtt5 support

--- worker.go
+++ worker.go
@@ -6,11 +6,13 @@ import (
 	"crypto/x509"
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
 	"strings"
 	"time"
 
-	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/eclipse/paho.golang/autopaho"
+	"github.com/eclipse/paho.golang/paho"
 )
 
 type PayloadGenerator func(i int) string
@@ -57,13 +59,13 @@ type Worker struct {
 	PauseBetweenMessages time.Duration
 }
 
-func setSkipTLS(o *mqtt.ClientOptions) {
-	oldTLSCfg := o.TLSConfig
+func setSkipTLS(o *autopaho.ClientConfig) {
+	oldTLSCfg := o.TlsCfg
 	if oldTLSCfg == nil {
 		oldTLSCfg = &tls.Config{}
 	}
 	oldTLSCfg.InsecureSkipVerify = true
-	o.SetTLSConfig(oldTLSCfg)
+	o.TlsCfg = oldTLSCfg
 }
 
 func NewTLSConfig(ca, certificate, privkey []byte) (*tls.Config, error) {
@@ -115,93 +117,110 @@ func (w *Worker) Run(ctx context.Context) {
 	subscriberClientId := fmt.Sprintf(subscriberClientIdTemplate, hostname, w.WorkerId, t)
 	publisherClientId := fmt.Sprintf(publisherClientIdTemplate, hostname, w.WorkerId, t)
 
+	burl, err := url.Parse(w.BrokerUrl)
+	if err != nil {
+		panic(err)
+	}
 	verboseLogger.Printf("[%d] topic=%s subscriberClientId=%s publisherClientId=%s\n", cid, topicName, subscriberClientId, publisherClientId)
-
-	publisherOptions := mqtt.NewClientOptions().SetClientID(publisherClientId).SetUsername(w.Username).SetPassword(w.Password).AddBroker(w.BrokerUrl)
-
-	subscriberOptions := mqtt.NewClientOptions().SetClientID(subscriberClientId).SetUsername(w.Username).SetPassword(w.Password).AddBroker(w.BrokerUrl)
-
-	subscriberOptions.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
-		queue <- [2]string{msg.Topic(), string(msg.Payload())}
-	})
-
+	sc := autopaho.ClientConfig{
+		BrokerUrls: []*url.URL{burl},
+		KeepAlive:  uint16(w.Timeout.Seconds()),
+		OnConnectionUp: func(cm *autopaho.ConnectionManager, _ *paho.Connack) {
+			verboseLogger.Printf("[%d] subscribing to topic\n", w.WorkerId)
+			if _, err := cm.Subscribe(ctx, &paho.Subscribe{Subscriptions: map[string]paho.SubscribeOptions{topicName: paho.SubscribeOptions{QoS: w.SubscriberQoS}}}); err != nil {
+				resultChan <- Result{
+					WorkerId:     w.WorkerId,
+					Event:        SubscribeFailedEvent,
+					Error:        true,
+					ErrorMessage: err,
+				}
+				return
+			}
+		},
+		OnConnectError: func(err error) {
+			resultChan <- Result{
+				WorkerId:     w.WorkerId,
+				Event:        ConnectFailedEvent,
+				Error:        true,
+				ErrorMessage: fmt.Errorf("sub: %s", err),
+			}
+			verboseLogger.Printf("[%d] connecting subscriber\n", w.WorkerId)
+		},
+		Debug: paho.NOOPLogger{},
+		ClientConfig: paho.ClientConfig{
+			ClientID:      subscriberClientId,
+			OnClientError: func(err error) { fmt.Printf("server requested disconnect: %s\n", err) },
+			OnServerDisconnect: func(d *paho.Disconnect) {
+				if d.Properties != nil {
+					fmt.Printf("server requested disconnect: %s\n", d.Properties.ReasonString)
+				} else {
+					fmt.Printf("server requested disconnect; reason code: %d\n", d.ReasonCode)
+				}
+			},
+			Router: paho.NewSingleHandlerRouter(func(msgpub *paho.Publish) {
+				queue <- [2]string{msgpub.Topic, string(msgpub.Payload)}
+			}),
+		},
+	}
+	sc.SetUsernamePassword(w.Username, []byte(w.Password))
 	if len(w.CA) > 0 || len(w.Key) > 0 {
 		tlsConfig, err := NewTLSConfig(w.CA, w.Cert, w.Key)
 		if err != nil {
 			panic(err)
 		}
-		subscriberOptions.SetTLSConfig(tlsConfig)
-		publisherOptions.SetTLSConfig(tlsConfig)
+		sc.TlsCfg = tlsConfig
 	}
 
 	if w.SkipTLSVerification {
-		setSkipTLS(publisherOptions)
-		setSkipTLS(subscriberOptions)
+		setSkipTLS(&sc)
 	}
 
-	subscriber := mqtt.NewClient(subscriberOptions)
-
-	verboseLogger.Printf("[%d] connecting subscriber\n", w.WorkerId)
-	if token := subscriber.Connect(); token.WaitTimeout(w.Timeout) && token.Error() != nil {
-		resultChan <- Result{
-			WorkerId:     w.WorkerId,
-			Event:        ConnectFailedEvent,
-			Error:        true,
-			ErrorMessage: token.Error(),
-		}
-
-		return
+	sub, err := autopaho.NewConnection(ctx, sc)
+	if err != nil {
+		panic(err)
 	}
-
 	defer func() {
 		verboseLogger.Printf("[%d] unsubscribe\n", w.WorkerId)
 
-		if token := subscriber.Unsubscribe(topicName); token.WaitTimeout(w.Timeout) && token.Error() != nil {
-			fmt.Printf("failed to unsubscribe: %v\n", token.Error())
+		if _, err := sub.Unsubscribe(ctx, &paho.Unsubscribe{Topics: []string{topicName}}); err != nil {
+			fmt.Printf("failed to unsubscribe: %v\n", err)
 		}
 
-		subscriber.Disconnect(5)
+		//subscriber.Disconnect(5)
 	}()
 
-	verboseLogger.Printf("[%d] subscribing to topic\n", w.WorkerId)
-	if token := subscriber.Subscribe(topicName, w.SubscriberQoS, nil); token.WaitTimeout(w.Timeout) && token.Error() != nil {
-		resultChan <- Result{
-			WorkerId:     w.WorkerId,
-			Event:        SubscribeFailedEvent,
-			Error:        true,
-			ErrorMessage: token.Error(),
-		}
-
-		return
-	}
-
-	publisher := mqtt.NewClient(publisherOptions)
-	verboseLogger.Printf("[%d] connecting publisher\n", w.WorkerId)
-	if token := publisher.Connect(); token.WaitTimeout(w.Timeout) && token.Error() != nil {
+	sc.ClientConfig.ClientID = publisherClientId
+	sc.OnConnectError = func(err error) {
 		resultChan <- Result{
 			WorkerId:     w.WorkerId,
 			Event:        ConnectFailedEvent,
 			Error:        true,
-			ErrorMessage: token.Error(),
+			ErrorMessage: fmt.Errorf("pub: %s", err),
 		}
-		return
+		verboseLogger.Printf("[%d] connecting publisher\n", w.WorkerId)
 	}
-
+	pub, err := autopaho.NewConnection(ctx, sc)
+	if err != nil {
+		panic(err)
+	}
 	verboseLogger.Printf("[%d] starting control loop %s\n", w.WorkerId, topicName)
 
 	stopWorker := false
 	receivedCount := 0
 	publishedCount := 0
-
+	pub.AwaitConnection(ctx)
+	sub.AwaitConnection(ctx)
 	t0 := time.Now()
 	for i := 0; i < w.NumberOfMessages; i++ {
 		text := w.PayloadGenerator(i)
-		token := publisher.Publish(topicName, w.PublisherQoS, w.Retained, text)
+		_, err = pub.Publish(ctx, &paho.Publish{Topic: topicName, QoS: w.PublisherQoS, Retain: w.Retained, Payload: []byte(text)})
+		if err != nil {
+			verboseLogger.Printf("[%d][%s] error publish: %s\n", w.WorkerId, topicName, err)
+		}
 		publishedCount++
-		token.WaitTimeout(w.Timeout)
 		time.Sleep(w.PauseBetweenMessages)
 	}
-	publisher.Disconnect(5)
+	//publisher.Disconnect(5)
 
 	publishTime := time.Since(t0)
 	verboseLogger.Printf("[%d] all messages published\n", w.WorkerId)
